25cdd42f88e0356e05fa0a8b86bd695afef1bf53
sergeyermakovich <sergu9848@gmail.com>
2026-02-09 23:34:31 +0300

    first commit

---
+++ src/main/java/tech/ermakovich/NewsAnalizerApplication.java
@@ -0,0 +1,13 @@
+package tech.ermakovich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAnalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAnalizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/ermakovich/NewsAnalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ermakovich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAnalizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
